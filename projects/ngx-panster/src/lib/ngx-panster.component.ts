8983f68d693c37c682d267bbc11dba3532650373
moodyVlad <56267705+moodyVlad@users.noreply.github.com>
2023-12-11 09:41:20 +0200

    Repo init

---
+++ projects/ngx-panster/src/lib/ngx-panster.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-panster',
+  standalone: true,
+  imports: [],
+  templateUrl: './ngx-panster.component.html',
+  styles: ``,
+})
+export class NgxPansterComponent {}
---
+++ projects/ngx-panster/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ngx-panster
+ */
+
+export * from './lib/ngx-panster.service';
+export * from './lib/ngx-panster.component';
